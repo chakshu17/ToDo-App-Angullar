532806b2f834108daf88183b8f6d49a1ffb83f19
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-30 16:37:05 +0530

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { AngularFireModule+ } from '@angular/fire' ;
+
 import { environment } from '../environments/environment';
+
 import { FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
